a77e484b6d26bbd541ad418a40129306cd5d77d7
IndaBasra <u5676792@live.warwick.ac.uk>
2024-11-23 20:01:21 +0000

    submit assignment

--- tests/test_line.cpp
+++ tests/test_line.cpp
@@ -4,4 +4,8 @@
 
 TEST_CASE("the length of a line to the origin is the same as the norm of a point from the origin", "[tests]")
 {
+	Point a(0.0, 0.0);
+	Point b(2.0, 3.0);
+	Line l(&a, &b);
+    REQUIRE(l.length() == b.compute_norm());
 }
--- tests/test_point.cpp
+++ tests/test_point.cpp
@@ -4,4 +4,9 @@
 
 TEST_CASE("adding a + a - a is a", "[tests]")
 {
+    Point a(1.0, 2.0);
+	Point r1 = a.add(&a);
+	Point r2 = r1.subtract(&a);
+    REQUIRE(a.x() == r2.x());
+    REQUIRE(a.y() == r2.y());
 }
